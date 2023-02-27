bed39f142ea0705c6a2870ae22fc2f2ac348ccd2
ChunyuBai <baichunyu26@gmail.com>
2023-02-27 14:02:06 -0500

    add login token

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getTokenFromUrl } from './authentication';
 import Login from './components/Login';
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+    console.log('mytoken =>', token);
+
+  }, []);
+
   return (
     <div className="App">
-      <Login />
-
+      {token ? <h1>login success</h1> : <Login/>}
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
 import React from "react";
 import './Login.css';
-import {loginUrl} from "../authentication"
+import {loginUrl} from "../authentication";
+
 function Login() {
+  
   return (
     <div className="login">
       <img src= "https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"/>
